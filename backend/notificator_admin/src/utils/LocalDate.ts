6f2d77eebe490c4d85387be67ac19d83db067601
ralexandear <ralexandear@gmail.com>
2025-07-08 11:31:08 +0200

    fixed localdate class

--- backend/notificator_admin/src/utils/LocalDate.ts
+++ backend/notificator_admin/src/utils/LocalDate.ts
@@ -1,7 +1,9 @@
+import { Configuration } from "../Configuration";
+
 export class LocalDate extends Date {
   constructor() {
     const args = Array.from(arguments) as ConstructorParameters<typeof Date>;
     const date = args.length ? new Date(...args) : new Date();
-    super(date.getTime());
+    super(date.getTime() + Configuration.timezone_offset * 60 * 1000);
   }
 }
